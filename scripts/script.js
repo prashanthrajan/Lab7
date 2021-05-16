de358e9e0aa80d5529b95735319ff3226b2117a4
prashanthrajan <60728661+prashanthrajan@users.noreply.github.com>
2021-05-16 10:52:59 -0700

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -6,7 +6,7 @@ const setState = router.setState;
 // Make sure you register your service worker here too
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('./sw.js').then((registration) => {
+    navigator.serviceWorker.register('/sw.js').then((registration) => {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
     }, (err) => {
