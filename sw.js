7e761a3ccd347319711b6fef0d869f3cd0388fad
ShaneVal <svalderr@ucsd.edu>
2021-05-16 01:44:07 -0700

    Part2 done

--- sw.js
+++ sw.js
@@ -4,3 +4,56 @@
 //   - One for installation
 //   - One for activation ( check out MDN's clients.claim() for this step )
 //   - One for fetch requests
+
+let CACHE_NAME = 'my-site-cache-v1';
+let urlsToCache = [
+  'https://cse110lab6.herokuapp.com/entries'
+];
+
+self.addEventListener('install', (event) => {
+    // Perform install steps
+    event.waitUntil(
+      caches.open(CACHE_NAME)
+        .then(function(cache) {
+          console.log('Opened cache');
+          return cache.addAll(urlsToCache);
+        })
+    );
+  });
+
+  self.addEventListener('fetch', (event) => {
+    event.respondWith(
+      caches.match(event.request)
+        .then((response) => {
+          // Cache hit - return response
+          if (response) {
+            return response;
+          }
+  
+          return fetch(event.request).then(
+            (response) => {
+              // Check if we received a valid response
+              if(!response || response.status !== 200 || response.type !== 'basic') {
+                return response;
+              }
+            // IMPORTANT: Clone the response. A response is a stream
+            // and because we want the browser to consume the response
+            // as well as the cache consuming the response, we need
+            // to clone it so we have two streams.
+              let responseToCache = response.clone();
+  
+              caches.open(CACHE_NAME)
+                .then((cache) => {
+                  cache.put(event.request, responseToCache);
+                });
+  
+              return response;
+            }
+          );
+        })
+      );
+  });
+
+  self.addEventListener('activate', (event) => {
+    event.waitUntil(clients.claim());
+  }); 
